2a2d109280df933f7da0f61dceb5aa0604c37c11
NizamUddinDev <nizam.cs.official@gmail.com>
2025-04-18 12:28:03 +0500

    Data types and commnets

--- BasicPythonConcepts/print.py
+++ BasicPythonConcepts/print.py
@@ -1,2 +1,26 @@
-#Hello world program
+#Hello world program 
 print("Hello world")
+print("\nString data type:")
+#variables are containers to store data  (integer, float, boolean, string)
+
+#String (Collection of characters)
+f_name="Nizam Uddin"
+print(f"Hello {f_name}") #f --> formate to output data
+print("\nInteger Data type:")
+#Integer (whole numbers)
+age=12
+quantity=5
+print(f"You are {age} years old")
+print(f"You have {quantity} dozen of mangoes")
+
+#Float point
+print("\n Float Data type")
+cgpa=3.3
+price=10.9
+print(f"price is ${price}")
+print("Your CGPA is {cgpa}")
+
+#Boolean
+print("\n Boolean Data")
+is_Student=True
+print(f"Are you student? {is_Student}")
